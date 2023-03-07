ddb8383a792669547d1db0e5b1af81582cccad14
LaveeshkaMahadea <laveeshka94@gmail.com>
2023-03-08 10:56:39 +1100

    add rediscachestore

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,11 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present? || ENV['RENDER'].present?
 
+  # Use a different cache store in production.
+config.cache_store = :redis_cache_store, {
+  url: ENV['REDIS_URL']
+}
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
